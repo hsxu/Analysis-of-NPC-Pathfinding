9dfad530c5a35d07262a20e07f12b0c37b2124cf
hsxu <harveyxuca@gmail.com>
2017-05-23 18:02:12 -0400

    Add aggression map and code

--- heatmap.py
+++ heatmap.py
@@ -1,7 +1,7 @@
 import plotly.plotly as py
 import plotly.graph_objs as go
 
-matrix = [[0 for x in xrange(70)] for y in xrange(70)]
+matrix = [[0 for x in xrange(75)] for y in xrange(75)]
 
 f = open('text.txt')
 for line in f:
@@ -10,7 +10,35 @@ for line in f:
     k = line.split(',')
     x = int(k[1])
     y = int(k[0])
-    matrix[x][y] += 1
+
+    matrix[x-2][y-2] += 1
+    matrix[x-1][y-2] += 1
+    matrix[x][y-2] += 1
+    matrix[x+1][y-2] += 1
+    matrix[x+2][y-2] += 1
+
+    matrix[x-2][y-1] += 1
+    matrix[x-1][y-1] += 1
+    matrix[x][y-1] += 1
+    matrix[x+1][y-1] += 1
+    matrix[x+2][y-1] += 1
+
+    matrix[x-2][y] += 1
+    matrix[x-1][y] += 1
+    matrix[x+1][y] += 1
+    matrix[x+2][y] += 1
+
+    matrix[x-2][y+1] += 1
+    matrix[x-1][y+1] += 1
+    matrix[x][y+1] += 1
+    matrix[x+1][y+1] += 1
+    matrix[x+2][y-2] += 1
+
+    matrix[x-2][y+2] += 1
+    matrix[x-1][y+2] += 1
+    matrix[x][y+2] += 1
+    matrix[x+1][y+2] += 1
+    matrix[x+2][y+2] += 1
 
 trace = go.Heatmap(z=matrix) 
 data=[trace]
